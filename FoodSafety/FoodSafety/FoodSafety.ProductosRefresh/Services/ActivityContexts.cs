7d652f21a334a7fd0465881f0a0f613f31853712
Pablo Ignacio <pab.garridom@gmail.com>
2019-05-18 13:09:00 -0400

    orden

--- FoodSafety/FoodSafety/FoodSafety.ProductosRefresh/Services/ActivityContexts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FoodSafety.ProductosRefresh.Services
-{
-    public static class ActivityContexts
-    {
-        public static HomeAgregaProductoActivity homeAddActivity { get; set; }
-        public static MainActivity mainActivity { get; set; }
-    }
-}
--- FoodSafety/FoodSafety/Services/ActivityContexts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoodSafety.Services
-{
-    public static class ActivityContexts
-    {
-        public static HomeAgregaProductoActivity homeAddActivity { get; set; }
-        public static HomeAgregaProductoActivity homeCtxActivity { get; set; }
-        public static MainActivity mainActivity { get; set; }
-    }
-}
